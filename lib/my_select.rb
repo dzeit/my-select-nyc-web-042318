890c4508330a8fce9518182cb4b8f8ad03257bbb
David Zeit <dzeit216@gmail.com>
2018-04-19 23:46:25 +0000

    automatically backed up by learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -2,29 +2,9 @@ def my_select(array)
   i = 0 
   new_array = []
   while i < array.length 
-  if yield(5)
+    if yield(array[i])
+    end
     i += 1 
   end 
   new_array
 end
-
-my_select(array) do |ele| 
-  ele < 30
-end
-
-array.each do |ele|
-  ele 
-end
-
-
-array.select do |n|
-  n.odd? 
-end 
-
-array.select do |n|
-  n > 10 
-end 
-
-array.select do |num|
-  num > 10 && num < 100
-end 
